e9ee45536580d7222e79978172ac589977f6fb44
fidheri <timfres3023@gmail.com>
2026-09-20 13:02:55 +0300

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import os
 import threading
 from flask import Flask
@@ -11,5 +10,11 @@ def health():
     return "Bot is alive", 200
 
 if __name__ == "__main__":
+    # Запускаем бота в отдельном потоке, чтобы он не мешал Flask
+    bot_thread = threading.Thread(target=bot.run_bot)
+    bot_thread.daemon = True
+    bot_thread.start()
+
+    # Запускаем веб-сервер (открываем порт для Render)
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
